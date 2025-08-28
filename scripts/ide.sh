4e39582afb114a8ef0a52cbd75a95da65218ac11
Amit <theamit.969@gmail.com>
2025-08-29 01:36:37 +0530

    tmux pane setup

---
+++ scripts/ide.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+tmux split-window -h -l 30%
+# tmux select-pane -L #focus the winidow to the left
+# tmux split-window -v -l 30%
+# tmux split-window -h -l 66%
+tmux split-window -v -l 35%
