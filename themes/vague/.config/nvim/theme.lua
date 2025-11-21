767b0a8b6ded4746ba49090166619c8223f97696
Amit <theamit.969@gmail.com>
2025-11-22 04:56:17 +0530

    fix

---
+++ themes/vague/.config/nvim/theme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "vague2k/vague.nvim",
+    priority = 1000,
+    opts = {
+      -- transparent = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vague",
+    },
+  },
+}
