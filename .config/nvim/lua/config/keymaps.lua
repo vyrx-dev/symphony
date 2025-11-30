045ca807791ea86965e75f3479b1479f3b2486b0
Amit <theamit.969@gmail.com>
2025-11-30 07:12:44 +0530

    keymaps: made changed

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,27 +1,54 @@
 -- Set leader key
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Keymap helper
 local set = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
+-- jj to escape insert mode
+set("i", "jj", "<Esc>", opts)
+
+-- vim.keymap.set("n", "<leader>wm", "<C-w>_|<C-w>|", { desc = "Maximize current window (no toggle)" })
+
 -- Disable the spacebar key's default behavior in Normal and Visual modes
-set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Split window
 set("n", "ss", ":split<Return>", opts)
 set("n", "sv", ":vsplit<Return>", opts)
 set("n", "sx", "<cmd>close<CR>", opts)
 
+-- Make current file executable
+vim.keymap.set("n", "<leader>X", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make current file executable" })
 
-set("n", "<leader>el", "<cmd>.lua<CR>", { desc = "Execute the current line" })
-set("n", "<leader>ef", "<cmd>source %<CR>", { desc = "Execute the current file" })
+-- Jumping
+set({ "n", "o", "x" }, "<s-h>", "^", { desc = "Jump to beginning of line" })
+set({ "n", "o", "x" }, "<s-l>", "g_", { desc = "Jump to end of line" })
+
+-- Copy file paths
+set("n", "<leader>cf", '<cmd>let @+ = expand("%")<CR>', { desc = "Copy File Name" })
 
 -- live-server
 set("n", "<leader>ps", ":LiveServerStart<CR>", { desc = "Start Live Server" })
 set("n", "<leader>pe", ":LiveServerStop<CR>", { desc = "Stop Live Server" })
-set("n", ";f", ":Oil<CR>", { desc = "oil explorer" })
+
+-- file-explorer
+set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Nvim tree " })
+
+-- harpoon
+-- local mark = require("harpoon.mark")
+-- local ui = require("harpoon.ui")
+--
+-- set("n", "<leader>ha", mark.add_file, opts) -- Harpoon add
+-- set("n", "<leader>h", ui.toggle_quick_menu, opts) -- Harpoon menu
+-- set("n", "hp", ui.nav_next, opts) -- Harpoon next file
+-- set("n", "hn", ui.nav_prev, opts) -- Harpoon previous
+--
+-- set("n", "<leader>1", function() ui.nav_file(1) end, opts)
+-- set("n", "<leader>2", function() ui.nav_file(2) end, opts)
+-- set("n", "<leader>3", function() ui.nav_file(3) end, opts)
+-- set("n", "<leader>4", function() ui.nav_file(4) end, opts)
 
 -- Center the screen after scrolling up/down with Ctrl-u/d
 set("n", "<C-u>", "<C-u>zz")
@@ -30,16 +57,16 @@ set("n", "n", "nzzzv", opts)
 set("n", "N", "Nzzzv", opts)
 
 -- Stay in indent mode
-set('v', '<', '<gv', opts)
-set('v', '>', '>gv', opts)
+set("v", "<", "<gv", opts)
+set("v", ">", ">gv", opts)
 
 -- Toggle line wrapping
-set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
+set("n", "<leader>lw", "<cmd>set wrap!<CR>", opts)
 
 -- save, quit
-set("n", "<leader>w", "<cmd> w <cr>" , opts)
-set("n", "<leader>sn", ":<cmd>noautocmd w <cr>" , opts)
-set("n", "<leader>c", "<cmd> q <cr>" ,opts)
+set("n", "<leader>w", "<cmd> w <cr>", opts)
+set("n", "<leader>sn", ":<cmd>noautocmd w <cr>", opts)
+-- set("n", "<leader>qq", "<cmd> q <cr>" ,opts)
 
 -- move a blocks of text up/down with K/J in visual mode
 set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
@@ -52,7 +79,7 @@ set("n", "<leader>vk", "<cmd>Screenkey<CR>", opts)
 set("n", "<C-a>", "gg<S-v>G")
 
 -- delete single character without copying into register
-set('n', 'x', '"_x', opts)
+set("n", "x", '"_x', opts)
 
 -- search and replace the word under cursor in the file with <leader>s
 set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
@@ -87,18 +114,14 @@ set("n", "<S-Tab>", ":bprevious<cr>", opts)
 set("n", "<leader>bd", ":bdelete!<cr>", opts) --close buffer
 set("n", "<leader>bn", "<cmd> enew <cr>", opts) -- new buffer
 
--- Diagnostic 
-set('n', '[d', function()
+-- Diagnostic
+set("n", "[d", function()
   vim.diagnostic.jump { count = -1, float = true }
-end, { desc = 'Go to previous diagnostic message' })
+end, { desc = "Go to previous diagnostic message" })
 
-set('n', ']d', function()
+set("n", "]d", function()
   vim.diagnostic.jump { count = 1, float = true }
-end, { desc = 'Go to next diagnostic message' })
+end, { desc = "Go to next diagnostic message" })
 
-set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
--- Save and load session
-set('n', '<leader>ss', ':mksession! .session.vim<CR>', { noremap = true, silent = false })
-set('n', '<leader>sl', ':source .session.vim<CR>', { noremap = true, silent = false })
+set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
